430e0af5dd87683b7bc087caba1349fb02166991
samtaplin <44102468+samtaplin@users.noreply.github.com>
2020-01-26 14:26:59 -0800

    fixed scoring, added AI opponent
    AI opponent currently uses regular AB tree search and a heuristic

--- azul/azul.py
+++ azul/azul.py
@@ -5,21 +5,36 @@ Created on Mon Dec 23 19:59:57 2019
 @author: Sam Taplin
 """
 import random
+import decision
 """Takes in manual input and returns the move chosen by the player"""
-def decidemove(playerboard, oppboard, factories, floor):
+def decidemove(playerboard, oppboard, factories, floor, game, midtake):
     chfact = input("Which factory do you want to pull from? (enter 'middle' for the middle, 1-5 for the others)")
     chcolor = input("Which color do you want to take?")
-    buck = input("Which bucket do you want to place it in? (1 for the smallest bucket, 5 fot the biggest)")
+    buck = input("Which bucket do you want to place it in? (1 for the smallest bucket, 5 fot the biggest, N for discarding)")
     if chfact == 'middle':
+        chind = len(factories)
         chfact = floor
     else:
+        chind = int(chfact) - 1
         chfact = factories[int(chfact) - 1]
     chcolor = int(chcolor)
-    buck = int(buck) - 1
-    return chfact, chcolor, playerboard.buckets[buck]
+    if buck != "N":
+        buck = int(buck) - 1
+        if decision.isLegal(chind, chcolor, buck, game, playerboard):
+            return chfact, chcolor, playerboard.buckets[buck]
+        else:
+            print("Cannot perform illegal move")
+            return decidemove(playerboard, oppboard, factories, floor, game, midtake)
+    else:
+        if decision.isLegal(chind, chcolor, buck, game, playerboard):
+            return chfact, chcolor, buck
+        else:
+            print("Cannot perform illegal move")
+            return decidemove(playerboard, oppboard, factories, floor, game, midtake)
 """A game of Azul. Includes the bag of tiles, the boards for each player and the factories that hold the tiles."""
 class Game:
     def __init__(self, size, players):
+        self.size = size
         self.bag = Bag(size)
         self.players = players
         self.board0 = Board(size)
@@ -47,29 +62,47 @@ class Game:
         else:
             return self.board0
     """Completes a turn for the current player in a two-player game. Returns True if the game ends."""
-    def taketurn2(self):
+    def taketurn2(self, p0movechoice, p1movechoice):
         currbo = self.goesfirst
         oppbo = self.other2(currbo)
         first = None
         while any([not fact.isempty() for fact in self.factories]) or not self.middle.isempty():
+            if currbo == self.board0:
+                thismove = p0movechoice
+            else:
+                thismove = p1movechoice
             print("Factories:")
             for i in range(len(self.factories)):
                 print(i + 1)
                 print(self.factories[i])
             print("Middle:")
             print(self.middle)
-            print("Opponent's board:")
+            print("Opponent's board:", oppbo.points)
             print(oppbo)
-            print("Your board:")
+            print("Your board:", currbo.points)
             print(currbo)
-            factory, color, bucket = decidemove(currbo, oppbo, self.factories, self.middle)
-            if not first and factory == self.middle:
+            if not first:
+                factory, color, bucket = thismove(currbo, oppbo, self.factories, self.middle, self, False)
+            else:
+                factory, color, bucket = thismove(currbo, oppbo, self.factories, self.middle, self, True)
+            if not first and factory == self.middle or factory == len(self.factories):
                 first = currbo
                 currbo.points = currbo.points - 1
+                print(currbo.points)
+            if type(factory) == int:
+                if factory == len(self.factories):
+                    factory = self.middle
+                else:
+                    factory = self.factories[factory]
+            if type(bucket) == int:
+                bucket = currbo.buckets[bucket]
             draw, tomid = factory.draw(color)
             self.middle.drop(tomid)
-            excess = bucket.insert(color, len(draw))
-            currbo.subtracts.extend(excess)
+            if bucket != "N":
+                excess = bucket.insert(color, len(draw))
+                currbo.subtracts.extend(excess)
+            else:
+                currbo.subtracts.extend(draw)
             oppbo = currbo
             currbo = self.other2(currbo)
         b0disc = self.board0.emptybuckets()
@@ -85,16 +118,63 @@ class Game:
         else:
             return False
     """Plays this game instance into completion."""
-    def playgame(self):
+    def playgame(self, p0movechoice, p1movechoice):
         over = False
         while not over:
-            over = self.taketurn2()
+            over = self.taketurn2(p0movechoice, p1movechoice)
             print("Player 0 points:")
             print(self.board0.points)
             print("Player 1 points:")
             print(self.board1.points)
             for fact in self.factories:
                 fact.fill(self.bag.drawfour())
+                
+    def generatesuccessor(self, move, player, midtake):
+        copy = Game(self.size, self.players)
+        copy.bag = self.bag.createcopy()
+        copy.board0 = self.board0.createcopy()
+        copy.board1 = self.board1.createcopy()
+        if self.goesfirst == self.board0:
+            copy.goesfirst = copy.board0
+        else:
+            copy.goesfirst = copy.board1
+        for i in range(len(self.factories)):
+            copy.factories[i] = self.factories[i].createcopy()
+        copy.middle = self.middle.createcopy()
+        for tile in self.discard:
+            copy.discard.append(tile.createcopy())
+        if player == self.board0:
+            cplayer = copy.board0
+        else:
+            cplayer = copy.board1
+        color = decision.getmovecolor(move)
+        faind = decision.getmovefact(move)
+        if not midtake and faind == len(self.factories):
+            copy.goesfirst = cplayer
+            cplayer.points = cplayer.points - 1
+            midtake = True
+        if faind == len(self.factories):
+            draw, tomid = copy.middle.draw(color)
+        else:
+            draw, tomid = copy.factories[faind].draw(color)
+        copy.middle.drop(tomid)
+        bucket = decision.getmovebucket(move)
+        if bucket != "N":
+            excess = cplayer.buckets[bucket].insert(color, len(draw))
+            cplayer.subtracts.extend(excess)
+        else:
+            cplayer.subtracts.extend(draw)
+        if all([fact.isempty() for fact in copy.factories]) and copy.middle.isempty():
+            b0disc = copy.board0.emptybuckets()
+            b1disc = copy.board1.emptybuckets()
+            copy.bag.adddiscard(b0disc)
+            copy.bag.adddiscard(b1disc)
+            if copy.board0.isendstate() or copy.board1.isendstate():
+                copy.board0.bonuspoints()
+                copy.board1.bonuspoints()
+            return copy, copy.goesfirst, copy.other2(copy.goesfirst), False
+        return copy, copy.other2(cplayer), cplayer, midtake
+        
 
 class Factory:
     def __init__(self):
@@ -117,6 +197,12 @@ class Factory:
             self.tiles = tiles
         else:
             raise GameError("Wrong number of tiles for a factory")
+    """Takes in a color and returns True if the factory contains at lease one tile of that color"""
+    def contains(self, color):
+        for tile in self.tiles:
+            if tile.getcolor() == color:
+                return True
+        return False
     """Returns True if the factory is empty. False if not."""
     def isempty(self):
         return not self.tiles
@@ -125,6 +211,13 @@ class Factory:
             return "|||X|||"
         else:
             return "<" + str([str(tile) for tile in self.tiles]) + ">"
+    
+    def createcopy(self):
+        copy = Factory()
+        for tile in self.tiles:
+            tcop = tile.createcopy()
+            copy.tiles.append(tcop)
+        return copy
 
 class MiddleFactory(Factory):
     """Draws all tiles of a color from the factory. Takes in a color and returns a list of tiles
@@ -144,6 +237,12 @@ class MiddleFactory(Factory):
         self.tiles.extend(tiles)
     def fill(self, tiles):
         raise GameError("Can't fill the middle of the floor")
+    def createcopy(self):
+        copy = MiddleFactory()
+        for tile in self.tiles:
+            tcop = tile.createcopy()
+            copy.tiles.append(tcop)
+        return copy
 class Board:
     
     def __init__(self, size):
@@ -158,29 +257,39 @@ class Board:
                 row.append(BoardTile((j - i) % size))
             self.tiles.append(row)
             self.buckets[i] = Bucket(i + 1)
-    """Copies another board into this board."""
-    def copy(self, board):
-        self.tiles = board.tiles
-        self.size = board.size
-        self.buckets = board.buckets
-        self.subtracts = board.subtracts
-        self.points = board.points
+    """Returns a copy of this board."""
+    def createcopy(self):
+        copy = Board(self.size)
+        for i in range(self.size):
+            for j in range(self.size):
+                copy.tiles[i][j] = self.tiles[i][j].createcopy()
+            copy.buckets[i] = self.buckets[i].createcopy()
+        for tile in self.subtracts:
+            copy.subtracts.append(tile.createcopy())
+        copy.points = self.points
+        return copy
     """Counts the number of filled columns in the board."""
     def countcolumns(self):
+        return self.countcolumnsofx(self.size)
+    def countcolumnsofx(self, x):
         full = 0
         for i in range(self.size):
-            if all([row[i].isfull() for row in self.tiles]):
+            if sum([row[i].isfull() for row in self.tiles]) == x:
                 full += 1
         return full
     """Counts the number of filled rows in the board."""
     def countrows(self):
+        return self.countrowsofx(self.size)
+    def countrowsofx(self, x):
         full = 0
         for row in self.tiles:
-            if all([tile.isfull() for tile in row]):
+            if sum([tile.isfull() for tile in row]) == x:
                 full += 1
         return full
     """Returns the number of sets of a color filled in a board."""
     def countofacolor(self):
+        return self.countxofacolor(self.size)
+    def countxofacolor(self, x):
         full = 0
         for i in range(self.size):
             colfl = 0
@@ -188,7 +297,7 @@ class Board:
                 tile = self.tiles[j][(j + i) % self.size]
                 if tile.isfull():
                     colfl += 1
-            if colfl == self.size:
+            if colfl == x:
                 full += 1
         return full
     """Adds bonus points to the board based on filled rows, columns and colors."""
@@ -205,7 +314,7 @@ class Board:
                 bcolor = bu.getcolor()
                 bu.empty()
                 col = (i + bcolor) % self.size
-                todiscard.extend([Tile(bcolor)] * (i - 1))
+                todiscard.extend([Tile(bcolor)] * (i))
                 self.tiles[i][col].fill()
                 row = self.tiles[i]
                 rowtot = 0
@@ -236,7 +345,7 @@ class Board:
                 if rowtot > 0 and coltot > 0:
                     totalpoints += 1
         todiscard.extend(self.subtracts)
-        numdisc = len(todiscard)
+        numdisc = len(self.subtracts)
         if numdisc < 3:
             pointdisc = 1 * numdisc
         elif numdisc < 6:
@@ -252,7 +361,10 @@ class Board:
         return self.countrows() > 0
     def __str__(self):
         rows = [str(row) + "\n" for row in self.tiles]
-        return "Buckets: " + str([str(self.buckets[i]) for i in range(self.size)]) + "\n Tiles: \n" + rows[0] + rows[1] + rows[2] + rows[3] + rows[4]
+        rowstr = ""
+        for row in rows:
+            rowstr += row
+        return "Buckets: " + str([str(self.buckets[i]) for i in range(self.size)]) + "\n Tiles: \n" + rowstr
                 
 class Tile:
     def __init__(self, color):
@@ -262,6 +374,8 @@ class Tile:
         return self.color
     def __repr__(self):
         return '|' + str(self.color) + "|"
+    def createcopy(self):
+        return Tile(self.color)
 class BoardTile(Tile):
     def __init__(self, color, filled=False):
         self.color = color
@@ -280,6 +394,8 @@ class BoardTile(Tile):
             return "|F" + str(self.color) + "|"
         else:
             return '|' + str(self.color) + "|"
+    def createcopy(self):
+        return BoardTile(self.color, self.filled)
 class Bucket:
     def __init__(self, size):
         self.size = size
@@ -304,9 +420,10 @@ class Bucket:
         elif self.color != color and self.color:
             return [Tile(color)] * amount
         elif self.size < self.holds + amount:
+            held = self.holds
             self.holds = self.size
             self.color = color
-            return [Tile(self.color)] * (self.holds + amount - self.size)
+            return [Tile(self.color)] * (held + amount - self.size)
     """Return the number of tiles currently in the bucket."""
     def getholds(self):
         return self.holds
@@ -315,6 +432,11 @@ class Bucket:
         return self.color
     def __str__(self):
         return "|_" + str(self.color) * self.holds + "X" * (self.size - self.holds)
+    def createcopy(self):
+        copy = Bucket(self.size)
+        if self.color != None:
+            copy.insert(self.holds, self.color)
+        return copy
 
 class Bag:
     def __init__(self, size):
@@ -348,10 +470,26 @@ class Bag:
         if len(self.squares) == 0:
             self.squares = self.discard
             random.shuffle(self.squares)
+    
+    def createcopy(self):
+        copy = Bag(0)
+        for tile in self.squares:
+            copy.squares.append(tile.createcopy())
+        for dtile in self.discard:
+            copy.discard.append(dtile.createcopy())
+        return copy
 class GameError(Exception):
     def __init__(self, message):
         self.message = message
-        
-g = Game(5, 2)
-g.playgame()
+
+def main():
+    option = input("Welcome to Azul! Enter 'AI' to play against the AI and 'MAN' to play against a manual player")
+    g = Game(5, 2)
+    if option == "AI":
+        g.playgame(decision.aimove, decidemove)
+    elif option == "MAN":
+        g.playgame(decidemove, decidemove)
+    else:
+        main()
+main()
         
---
+++ azul/decision.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  7 22:58:14 2020
+
+@author: sctap
+"""
+def getmovefact(move):
+    return move[0]
+def getmovecolor(move):
+    return move[1]
+def getmovebucket(move):
+    return move[2]
+def LegalMoves(game, playerboard):
+    for faind in range(len(game.factories) + 1):
+        for co in range(game.size):
+            for buind in range(game.size):
+                if isLegal(faind, co, buind, game, playerboard):
+                    yield (faind, co, buind)
+            yield (faind, co, "N")
+def move(factory, color, bucket):
+    return (factory, color, bucket)
+    
+def aimove(playerboard, oppboard, factories, floor, game, midtake=False, depth=3):
+    m = None
+    mv = -float("inf")
+    a = -float("inf")
+    b = float("inf")
+    for move in LegalMoves(game, playerboard):
+        successor, nextpb, noppb, nmidtake = game.generatesuccessor(move, playerboard, midtake)
+        v = value(successor, nextpb, noppb, nmidtake, depth, a, b)
+        if v > mv:
+            mv = v
+            m = move
+        a = max(a, v)
+    return m
+def value(game, playerboard, oppboard, midtake, depth, a, b):
+    #if playerboard == game.board0:
+    #    depth = depth - 1
+    depth = depth - 1
+    if playerboard.isendstate() or oppboard.isendstate():
+        return evaluation(game, playerboard)
+    elif depth == 0:
+        return evaluation(game, playerboard)
+    elif playerboard == game.board0:
+        return maxp0(game, playerboard, oppboard, midtake, depth, a, b)
+    else:
+        return minp1(game, playerboard, oppboard, midtake, depth, a, b)
+def maxp0(game, playerboard, oppboard, midtake, depth, a, b):
+    v = -float("inf")
+    for move in LegalMoves(game, playerboard):
+        successor, nextpb, noppb, nmidtake = game.generatesuccessor(move, playerboard, midtake)
+        v = max(v, value(successor, nextpb, noppb, nmidtake, depth, a, b))
+        if v > b:
+            return v
+        a = max(a, v)
+    return v
+    
+def minp1(game, playerboard, oppboard, midtake, depth, a, b):
+    v = float("inf")
+    for move in LegalMoves(game, playerboard):
+        successor, nextpb, noppb, nmidtake = game.generatesuccessor(move, playerboard, midtake)
+        v = min(v, value(successor, nextpb, noppb, nmidtake, depth, a, b))
+        if v < a:
+            return v
+        b = min(b, v)
+    return v    
+def evaluation(game, playerboard):
+    if game.board0.isendstate() or game.board1.isendstate():
+        if game.board0.points > game.board1.points:
+            return float("inf")
+        elif game.board0.points < game.board1.points:
+            return -float("inf")
+        elif game.board0.countrows() > game.board1.countrows():
+            return float("inf")
+        elif game.board0.countrows() < game.board1.countrows():
+            return -float("inf")
+    game.board0.bonuspoints()
+    game.board1.bonuspoints()
+    pointspread = game.board0.points - game.board1.points
+    b04ofac = game.board0.countxofacolor(4)
+    b04col = game.board0.countcolumnsofx(4)
+    b04row = game.board0.countrowsofx(4)
+    b14ofac = game.board1.countxofacolor(4)
+    b14col = game.board1.countcolumnsofx(4)
+    b14row = game.board1.countrowsofx(4)
+    b0bucketsfull = sum([bu.isfull() for bu in game.board0.buckets.values()])
+    b1bucketsfull = sum([bu.isfull() for bu in game.board1.buckets.values()])
+    pointspread += b0bucketsfull * max(b04col, b04row, game.board0.countrows(), game.board0.countcolumns(), 1)
+    pointspread -= b1bucketsfull * max(b14col, b14row, game.board1.countrows(), game.board1.countcolumns(), 1)
+    pointspread += b0bucketsfull * 2 * (b04row / 5)
+    pointspread += b0bucketsfull * 7 * (b04col / 5)
+    pointspread += b0bucketsfull * 10 * (b04ofac / 5)
+    pointspread -= b1bucketsfull * 2 * (b14row / 5)
+    pointspread -= b1bucketsfull * 7 * (b14col / 5)
+    pointspread -= b1bucketsfull * 10 * (b14ofac / 5)
+    numdisc = len(game.board0.subtracts)
+    if numdisc < 3:
+        pointdisc = 1 * numdisc
+    elif numdisc < 6:
+        pointdisc = 2 + (2 * (numdisc - 2))
+    else:
+        pointdisc = 8 + (3 * (numdisc - 5))
+    pointspread -= pointdisc
+    numdisc1 = len(game.board1.subtracts)
+    if numdisc < 3:
+        pointdisc1 = 1 * numdisc1
+    elif numdisc < 6:
+        pointdisc1 = 2 + (2 * (numdisc1 - 2))
+    else:
+        pointdisc1 = 8 + (3 * (numdisc1 - 5))
+    pointspread += pointdisc1
+    return pointspread
+def isLegal(faind, color, bucket, game, playerboard):
+    if faind == len(game.factories):
+        fact = game.middle
+    else:
+        fact = game.factories[faind]
+    if not fact.isempty() and fact.contains(color):
+        if bucket == "N":
+            return True
+        bucketcol = playerboard.buckets[bucket].getcolor()
+        if bucketcol != None and bucketcol != color:
+            return False
+        for tile in playerboard.tiles[bucket]:
+            if tile.getcolor() == color and tile.isfull():
+                return False
+            elif tile.getcolor() == color:
+                return True
+        
+    else:
+        return False
